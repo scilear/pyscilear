19eaf7be2994d124ee9482a27790a2e86f30631a
scilear <scilear@gmail.com>
2016-10-14 10:40:08 +0200

    added done_processing stamp to mark_job

--- pyscilear/jobs.py
+++ pyscilear/jobs.py
@@ -1,8 +1,8 @@
 from logbook import info
 
 from pyscilear.pg import upsert
-
+import datetime
 
 def mark_job(job_id, state):
     info('job %d is completed state = %s' % (job_id, state))
-    upsert('jobs').row({'id': job_id}, {'state': state})
+    upsert('jobs').row({'id': job_id}, {'state': state, 'done_processing':datetime.datetime.now()})

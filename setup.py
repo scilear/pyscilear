707d8a540b3bc786e864379da5fdfd9abd6ad287
scilear <scilear@gmail.com>
2016-12-13 17:29:30 +0100

    add job splitter kickoff and wait added email sending function

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyscilear',
-      version='2016.11.21',
+      version='2016.12.13',
       description='scilear utilities',
       url='https://github.com/scilear/pyscilear.git',
       author='Scilear',
